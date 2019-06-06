96b2367936277861e84d2850dbd2da8497009b7b
Misty DeRamus <mistyderamus@gmail.com>
2019-06-06 14:14:22 -0500

    passing arguments

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
-let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+let sentence = [
+  "The",
+  "walrus",
+  "danced",
+  "through",
+  "the",
+  "trees",
+  "in",
+  "the",
+  "light",
+  "of",
+  "the",
+  "moon"
+];
 
-function addExcitement (theWordArray) {
-    let buildMeUp = ""
+function addExcitement(theWordArray, character) {
+  let buildMeUp = "";
 
-    for (let i = 0; i < theWordArray.length; i++) {
-        // console.log('value i:',i,'value i+1:',i+1,'value i % 3:',i%3,'value i+1%3:',(i + 1) %3);
-        if ([i + 1] % 3 == 0) { //add 1 to i to start ! at third word
-            buildMeUp += theWordArray[i] + "!"; 
-        } else {
-        buildMeUp += theWordArray[i] + " ";
-        }
-        console.log(buildMeUp);
-    
-    } 
+  for (let i = 0; i < theWordArray.length; i++) {
+    // console.log('value i:',i,'value i+1:',i+1,'value i % 3:',i%3,'value i+1%3:',(i + 1) %3);
+    if ([i + 1] % 3 == 0) {
+      //add 1 to i to start ! at third word
+      buildMeUp += theWordArray[i] + `${character}`;
+    } else {
+      buildMeUp += theWordArray[i] + " ";
+    }
+    console.log(buildMeUp);
+  }
 }
 
-
-
-addExcitement(sentence)
+addExcitement(sentence, `?`);
